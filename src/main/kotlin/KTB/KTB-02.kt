fa7fe7c550d3a430520d13f5f94c49fcc7cd784a
ramoart <donni.lab.1@gmail.com>
2025-12-30 02:06:45 -0500

    KTB-02 commit 30.12.25, please approve the pull request

--- src/main/kotlin/KTB/KTB-02.kt
+++ src/main/kotlin/KTB/KTB-02.kt
@@ -3,13 +3,9 @@ package KTB
 import java.io.File
 
 fun main() {
-    val wordsFile: File = File("src/main/kotlin/KTB/words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка")
+    val wordsFile = File("words.txt")
 
-    for (i in wordsFile.readLines()) {
-        println(i)
+    for (word in wordsFile.readLines()) {
+        println(word)
     }
 }
