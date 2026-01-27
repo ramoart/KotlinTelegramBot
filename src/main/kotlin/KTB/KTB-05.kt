7d5202bf984fcff9337f72996bc58cc88786f8dd
ramoart <donni.lab.1@gmail.com>
2026-01-27 02:49:25 -0500

    KTB-05 commit 27.01.26, please approve the pull request

---
+++ src/main/kotlin/KTB/KTB-05.kt
@@ -0,0 +1,37 @@
+package KTB
+
+import java.io.File
+
+fun main() {
+    val dictionary = loadDictionary()
+
+    while (true) {
+        println(
+            "Меню:\n" +
+                    "1 - Учить слова\n" +
+                    "2 - Статистика\n" +
+                    "0 - Выход"
+        )
+        val inputNumber = readln().toInt()
+        when {
+            inputNumber == 1 -> println("Выбран пункт Учить слова")
+            inputNumber == 2 -> println("Выбран пункт Статистика")
+            inputNumber == 0 -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+}
+
+fun loadDictionary(): MutableList<Word> {
+    val wordsFile = File("words.txt")
+    val dictionary = mutableListOf<Word>()
+
+    for (line in wordsFile.readLines()) {
+        val split = line.split('|')
+        val correctAnswersCount = split.getOrNull(2)?.toInt() ?: 0
+        val word = Word(split.getOrNull(0) ?: "", split.getOrNull(1) ?: "", correctAnswersCount)
+        dictionary.add(word)
+    }
+
+    return dictionary
+}
